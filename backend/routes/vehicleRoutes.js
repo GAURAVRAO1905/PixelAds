965f6bed61f3577a00e920f7d9054097aa07ce2f
Rakshit <62815614+rakshit2508@users.noreply.github.com>
2024-07-03 23:43:52 +0530

    Update vehicleRoutes.js

--- backend/routes/vehicleRoutes.js
+++ backend/routes/vehicleRoutes.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import {vehicleregister} from '../controllers/vehicleController.js'
+import {vehicleregister} from '../controllers/vehicleController.js';
+import { protect } from '../middleware/authMiddleware.js';
 
 
 const router = express.Router();
 
 // POST /api/campaign/create - Create a new campaign
-router.post('/register', vehicleregister);
+router.post('/register',protect, vehicleregister);
 
 
 export default router;
